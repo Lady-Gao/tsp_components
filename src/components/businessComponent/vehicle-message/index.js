6a20836ed8553c82de921f9ffa44265b46e732bc
张婵玉 <zhangchanyu@zhangchanyudeMacBook-Pro.local>
2019-11-21 09:40:01 +0800

    整理组件

--- src/components/businessComponent/vehicle-message/index.js
+++
@@ -1,8 +0,0 @@
-
-import VehicleMessage from './src/vehicle-message.vue'
-
-VehicleMessage.install = function (Vue) {
-    Vue.component(VehicleMessage.name, VehicleMessage);
-}
-
-export default VehicleMessage;
