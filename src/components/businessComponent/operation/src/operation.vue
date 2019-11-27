40875cd8ab9dabd7554c0be74bdfd5f4c255197d
张婵玉 <zhangchanyu@zhangchanyudeMacBook-Pro.local>
2019-11-27 16:25:35 +0800

    修改operation组件

--- src/components/businessComponent/operation/src/operation.vue
+++ src/components/businessComponent/operation/src/operation.vue
@@ -1,7 +1,10 @@
 <template>
-    <el-form :inline="true" 
-    :class="['cv-operation', position=='right'?'cv-operation-right': 'cv-operation-left']">
+    <el-form :inline="true"  :class="['cv-operation', position=='right'?'cv-operation-right': 'cv-operation-left']">
         <slot></slot>
+        <span v-if="$slots.header" class="toggle" @click="showItem">{{flag ? '收起' : '展开'}}<i :class="flag?'el-icon-arrow-up' :'el-icon-arrow-down'"></i></span>
+        <div :class="flag?'blank':''">
+            <slot name="header" v-if="flag"></slot>
+        </div>
     </el-form>
 </template>
 
@@ -14,17 +17,40 @@ export default {
             type: String,
             default: 'left'
         }
+    },
+    data(){
+        return {
+            flag:false
+        }
+    },
+    methods:{
+        showItem(){
+            this.flag=!this.flag;
+        }
     }
 }
 </script>
 
 <style lang="scss">
+.cv-operation{
     .cv-operation-left {
         text-align: left;
     }
     .cv-operation-right {
         text-align: right;
     }
+    .blank{
+        padding-top: 10px;
+        border-top: 1px dashed  #E9E9E9;
+    }
+    .toggle{
+        font-size: 14px;
+        color: #1890FF;
+        cursor: pointer;
+        float:right;
+        margin: 7px 20px 0px 0px;
+    }
+}  
 </style>
 
 
