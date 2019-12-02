a6cab9bc663a0fb03ba15e76a6f550b8158e859b
张婵玉 <zhangchanyu@zhangchanyudeMacBook-Pro.local>
2019-12-02 10:48:34 +0800

    修改组件

--- index.js
+++ index.js
@@ -1,96 +1,15 @@
-import Grid from "./src/components/grid/index"
-import CheckboxLists from "./src/components/checkLists/index"
-import Ocx from "./src/components/ocx/index";
-import H5Video from "./src/components/h5Video/index";
-import FlashVideo from "./src/components/flashVideo/index";
-import RadioLists from "./src/components/radioLists/index";
-import ThemePicker from "./src/components/themePicker/index";
-import GaodeMap from "./src/components/gaode/index";
-import BaiduMap from "./src/components/baidu/index";
-import GoogleMap from "./src/components/google/index";
-import LazyRender from "./src/components/lazyRender/index";
-import Icon from "./src/components/icon/index";
-import BreadCrumb from "./src/components/breadCrumb/index"
-import MapArea from './src/components/mapArea/index';
-import Scrollbar from "./src/components/scrollbar/index";
-import Operation from "./src/components/operation/index"
-import PowerTool from "./src/components/powerTool/index"
-import TableTool from "./src/components/tableTool/idnex"
-import Dropdown from "./src/components/dropDown/index"
-import BodyContent from "./src/components/bodyContent/index"
-import Tabs from "./src/components/tabs/index"
-import Dialogdrag from "./src/components/dialogdrag/index"
-import TabsTree from "./src/components/tabs-tree/index"
-import Tree from "./src/components/tree/index"
-import DropdownTree from "./src/components/dropdown-tree/index"
-import Maptool from "./src/components/mapTool/index"
-
-import PasswordStrength from "./src/components/passwordStrength/index"
-const components = [
-    Dropdown,
-    Grid,
-    Ocx,
-    H5Video,
-    FlashVideo,
-    RadioLists,
-    ThemePicker,
-    Scrollbar,
-    PowerTool,
-    TableTool,
-    BodyContent,
-    PasswordStrength,
-    GaodeMap,
-    BaiduMap,
-    GoogleMap,
-    LazyRender,
-    Maptool,
-    Icon,
-    TabsTree,
-    BreadCrumb,
-    MapArea,
-    CheckboxLists,
-    Operation,
-    Tabs,
-    Dialogdrag,
-    DropdownTree,
-    Tree
-]
-const install = function (Vue) {
-    console.log(components)
-    components.map(component => {
-        Vue.component(component.name, component);
-    });
-
-};
+/**
+ * https://webpack.js.org/guides/dependency-management/#requirecontext
+ * 不需要再使用Import方式来引用
+ * 这里会require所有的模块从“./modules”目录下，关于require.context是webpack的api。可参考上面链接
+ * 里面包含了resolve、keys、id三个属性
+ * 如果分了多级目录，那么第二个参数为true
+ */
+const modulesFiles = require.context("./src/components", true, /\.js$/);
+const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+    console.log(modules,modulePath)
+    return modules;
+}, {});
 export default {
-    version: '0.1.0',
-    install,
-    Dropdown,
-    Grid,
-    Ocx,
-    BodyContent,
-    H5Video,
-    FlashVideo,
-    PasswordStrength,
-    RadioLists,
-    ThemePicker,
-    Scrollbar,
-    TableTool,
-    PowerTool,
-    // Tabs,
-    GaodeMap,
-    BaiduMap,
-    GoogleMap,
-    LazyRender,
-    Maptool,
-    Icon,
-    TabsTree,
-    BreadCrumb,
-    MapArea,
-    CheckboxLists,
-    Operation,
-    Tabs,
-    Dialogdrag,
-    DropdownTree,
-    Tree
+    modules
 }
